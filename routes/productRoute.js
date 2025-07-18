8b23d1766afce27ae4f3c3ef175f301ccde8452f
Sakshiudagatti <sakshiudagatti28@gmail.com>
2025-07-18 15:07:09 +0530

    backend

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,7 @@
 const express=require("express")
 
 const router=express.Router();
-const {createProduct, getAllProducts, getProductById, updatedProductById, deleteProductById}= require("../controller/productcontroller")
+const {createProduct, getAllProducts, getProductById, updatedProductById, deleteProductById}= require("../controller/productController")
 const {auth} = require("../middleware/auth");
 const upload=require("../middleware/upload");
 router.post("/", auth, upload.single("image"),createProduct)
